27287bf11d27f83e4603afdde93609c878c1ebed
SundarMasetty <166877318+SundarMasetty@users.noreply.github.com>
2024-11-21 17:11:00 -0500

    Update main.py
    dtd

--- main.py
+++ main.py
@@ -261,6 +261,6 @@ def image_details(filename):
                            description=description)
 
 if __name__ == '__main__':
-
-    #app.run(debug=True)
     app.run(host="0.0.0.0", port=8080)
+    #app.run(debug=True)
+    
